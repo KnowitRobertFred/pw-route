e07586309e1a739da49e302abd3f556dc6bc2a76
Robert Fred <robert.fred@knowit.se>
2024-11-11 14:27:17 +0100

    * Lösningsförslag.

--- tests/search.spec.ts
+++ tests/search.spec.ts
@@ -1,12 +1,51 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, request } from '@playwright/test';
+import * as fs from 'fs';
 
 test('Route search', async ({ page }) => {
+  // Manipulera anrop
+  // await page.route('**/getGlobalSearchHits*', async (route, request) => {
+  //   const myUrl = request.url();
+  //   const urlObj = new URL(myUrl);
+  //   urlObj.searchParams.set('query', 'Solutions');
+
+  //   await route.continue({ url: urlObj.toString() });
+  // });
+
+  // Avbryt anrop (bara för att)
+  // await page.route('**/getGlobalSearchHits*', async (route) => {
+  //   await route.abort();
+  // });
+
+  // Svara med hårdkodat anrop
+  // await page.route('**/getGlobalSearchHits*', async (route) => {
+  //   const globalSearchHits = fs.readFileSync('mock-data/globalSearchHits.json');
+  //   await route.fulfill({ body: globalSearchHits });
+  // });
+
+  // Manipulera faktiskt svar
+  // await page.route('**/getGlobalSearchHits*', async (route) => {
+  //   const realResponse = await route.fetch();
+  //   const body = await realResponse.json();
+  //   const firstHit = body.searchHits[0];
+
+  //   const myResponse = {
+  //     searchHits: [firstHit],
+  //     totalHits: 1,
+  //     hasZeroHits: false,
+  //     nextPage: null,
+  //   };
+
+  //   await route.fulfill({
+  //     json: myResponse,
+  //   });
+  // });
+
   await page.goto('/');
   await page.getByText('Neka alla', { exact: true }).click();
   await page.getByText('Sök', { exact: true }).click();
   await page.locator('[name="q"]').fill('Test');
   await page.keyboard.press('Enter');
   await expect(page.locator('.SearchPage h2.chakra-heading')).toContainText(
-    '81 träffar på Test'
+    '1 träffar på Test'
   );
 });
